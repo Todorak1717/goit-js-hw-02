d398a677a288f6228b5c44e317a9f29702b69427
Todorak1717 <155604203+Todorak1717@users.noreply.github.com>
2024-04-15 22:33:56 -0500

    up

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+function makeTransaction (quantity, pricePerDroid, customerCredits){
+    let totalPrice = quantity * pricePerDroid;
+
+    if (totalPrice > customerCredits){
+        console.log ('Insufficient funds!')
+        {else ('You ordered $(quantity) droids worth $(totalPrice) credits!')
+        
+    }
+}
+}
